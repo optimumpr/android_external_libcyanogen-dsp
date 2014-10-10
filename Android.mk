d7f871475c884229734d05f7623d14b8048d35ee
Christopher N. Hesse <raymanfx@gmail.com>
2014-10-10 23:44:55 +0200

    libcyanogen-dsp: initial import from DSPManager
    Change-Id: I2b8db7fe131675abc9c5d63f1d9c357e11e9ae1d

---
+++ Android.mk
@@ -0,0 +1,42 @@
+LOCAL_PATH := $(call my-dir)
+
+
+ifneq ($(TARGET_ENABLE_DIRAC_AUDIO),true)
+include $(CLEAR_VARS)
+LOCAL_MODULE := audio_effects.conf
+LOCAL_SRC_FILES := audio_effects_cmdsp.conf
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT)/vendor/etc
+include $(BUILD_PREBUILT)
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcyanogen-dsp
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/soundfx
+LOCAL_PRELINK_MODULE := false
+LOCAL_ARM_MODE := arm
+LOCAL_SRC_FILES := \
+	cyanogen-dsp.cpp \
+	Biquad.cpp \
+	Delay.cpp \
+	Effect.cpp \
+	EffectBassBoost.cpp \
+	EffectCompression.cpp \
+	EffectEqualizer.cpp \
+	EffectVirtualizer.cpp \
+	FIR16.cpp \
+# terminator
+
+LOCAL_C_INCLUDES += \
+	frameworks/base/include \
+	hardware/libhardware/include \
+	system/core/include \
+	system/media/audio_effects/include \
+# terminator
+
+LOCAL_SHARED_LIBRARIES := \
+	libcutils
+
+include $(BUILD_SHARED_LIBRARY)
